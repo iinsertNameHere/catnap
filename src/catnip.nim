b67aba7dbc34fa76347e4dc1d9eb0e1b534d750a
iinsertNameHere <iinsert.mailhere@gmail.com>
2024-03-11 21:14:02 +0100

    Help now lists supported distros

--- src/catnip.nim
+++ src/catnip.nim
@@ -2,6 +2,13 @@ import "catniplib/fetch"
 import "catniplib/drawing/render"
 import os
 from unicode import toLower
+from "catniplib/common/defs" import CONFIGPATH
+import "catniplib/common/config"
+import "catniplib/common/toml"
+
+proc getAllDistros(): seq[string] =
+    for distro in LoadConfig(CONFIGPATH).distroart.getTable().keys:
+        result.add(distro)
 
 # Debug code for execution time
 when not defined release: 
@@ -26,6 +33,9 @@ if paramCount() > 0:
             echo ""
             echo "StatNames:"
             echo "    username, hostname, uptime, distro, kernel, desktop, shell"
+            echo ""
+            echo "DistroIds:"
+            echo "    " & getAllDistros().join(", ")
             echo ""
             quit()
 
