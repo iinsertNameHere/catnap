dfb06b39bacfe8551e6615a4012537774cf44486
iinsertNameHere <iinsert.mailhere@gmail.com>
2024-05-23 22:20:46 +0200

    Added 'Test all stats' to tests

--- scripts/test-commandline-args.sh
+++ scripts/test-commandline-args.sh
@@ -1,26 +1,39 @@
 # Test help
+echo "[!] Testing: Help"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -h
 
 # Test version
+echo "[!] Testing: Version"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -v
 
 # Test distroid
+echo "[!] Testing: DistroId"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -d arch
 
 # Test grep
+echo "[!] Testing: Grep"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -g kernel
 
 # Test margin
+echo "[!] Testing: Margin"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -m 1,2,3
 
 # Test layout
+echo "[!] Testing: Layout"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -l ArtOnTop
 
 # Test figletlogos mode
+echo "[!] Testing: FigletLogos"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -fe on
 
 # Test figletlogos margin
+echo "[!] Testing: FigletLogos Margin"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -fe on -fm 1,2,3
 
 # Test figletlogos font with example figlet font file
+echo "[!] Testing: FigletLogos Font"
 ./../bin/catnip -c ../config/config.toml -a ../config/distros.toml -fe on -ff basic.flf
+
+# Test every stat
+echo "[!] Testing: Every Stat"
+./../bin/catnip -c ./test_config.toml -a ../config/distros.toml
